d8d718d20384a3a3ec774a644a7bd1b504e62b05
Usman Ali <usman_ali545@yahoo.com>
2020-09-22 17:52:21 +0300

    get some data

---
+++ components/Loading.js
@@ -0,0 +1,7 @@
+// import { Loading as CarbonLoading } from 'carbon-components-react'
+
+import { Loading as CarbonLoading } from 'carbon-components-react'
+
+export default function Loading() {
+  return <CarbonLoading />
+}
--- pages/date/[date].js
+++ pages/date/[date].js
@@ -1,3 +1,53 @@
-export default function SportsOnDate() {
+import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+
+const LoadingNoSSR = dynamic(() => import('components/Loading'), {
+  ssr: false,
+  loading: () => <div>...loading</div>,
+})
+
+export default function SportsOnDate({ data }) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <LoadingNoSSR />
+  }
+
+  if (!data || !data.length) {
+    return <div>sorry </div>
+  }
+
   return <div>SportsOnDate</div>
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { date: '' } }, // See the "paths" section below
+    ],
+    fallback: true, // or false // See the "fallback" section below
+  }
+}
+
+export async function getStaticProps(context) {
+  const date = context?.params?.date
+  const url = `${process.env.API_URL}/events?accept=json&date=${date}&days=2`
+
+  const res = await fetch(url)
+  let data = null
+  if (res.ok) {
+    data = await res.json()
+  }
+
+  if (!data || !data.length) {
+    return {
+      props: {}, // will be passed to the page component as props
+      revalidate: 1,
+    }
+  }
+
+  return {
+    props: { data }, // will be passed to the page component as props
+    revalidate: 1,
+  }
 }
